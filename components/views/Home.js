b7c1f10ee831134670470f32f76dae6631fe8cbf
Vontay777 <lavonte.wallace7@gmail.com>
2023-10-16 21:28:32 -0500

    week 8 hw

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,9 +1,9 @@
 import html from "html-literal";
 
 export default state => html`<section id = "jumbotron">
-<h1>Find a trail <span style="display: inline-block; font-size: 100px; color: #000000;"> 𓃥</span></h1>
+<h2>Find a trail <span style="display: inline-block; font-size: 200px; color: #736f4e;"> 𓃥</span></h2>
 <a href="Trails.js">"𓃠"</a>
-<h2>
+<h3>
     The weather in ${state.weather.city} is ${state.weather.description}. Temperature is ${state.weather.temp}F, and it feels like ${state.weather.feelsLike}F.
-  </h2>
+  </h3>
 </section `;
---
+++ server/app.js
@@ -0,0 +1,92 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+app.get("/status", (request, response) => {
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+
+const PORT = process.env.PORT || 4040;
+
+// not sure if needed //
+
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+
+app.get("/status", (request, response) => {
+  response.status(200).json({ message: "Service healthy" });
+});
+
+app.get("/weather/:city", (request, response) => {
+  const city = request.params.city;
+
+  let cloudy = "clear";
+  let rainy = false;
+  let lowTemp = 32;
+
+  if ("cloudy" in request.query) {
+    cloudy = request.query.cloudy;
+  }
+  if ("rainy" in request.query && request.query.rainy === "true") {
+    rainy = request.query.rainy;
+  }
+  if ("lowtemp" in request.query) {
+    lowTemp = Number(request.query.lowtemp);
+  }
+  const min = 70;
+  const max = 90;
+  const temp = Math.floor(Math.random() * (max - min + 1) + min);
+  response.send(
+    JSON.stringify({
+      current: `The weather in ${city} is ${temp} degrees today.`,
+      cloudy: cloudy,
+      rainy,
+      temp: {
+        current: temp,
+        low: lowTemp
+      }
+    })
+  );
+});
+
+// not sure ^^^ //
+
+app.listen(4040, () => console.log(`Listening on port ${PORT}`));
