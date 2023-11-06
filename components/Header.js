18b499b6373b91cc52b6cb19b419bf5aac826252
Vontay777 <lavonte.wallace7@gmail.com>
2023-11-06 14:35:14 -0600

    final stages

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,5 @@ import html from "html-literal";
 export default state => html`
   <header>
     <h1>${state.header}</h1>
-    <img src="./assets/img/logocap1.png" class="logo" />
   </header>
 `;
--- components/views/Contact.js
+++ components/views/Contact.js
@@ -8,6 +8,26 @@ export default state => html`
       action=""
       onsubmit="return confirm('Thank you! Will be sure to get back to you!');"
     >
+      <div>
+        <h2>
+          About me
+        </h2>
+        <div id="po">
+          <p>
+            I am an aspiring full-stack developer specialising in web
+            application construction using HTML, CSS, and JavaScript while
+            following agile methodologies to maintain focus on projects.
+            Passionate about continuously growing and furthering my knowledge
+            through experience.
+          </p>
+        </div>
+      </div>
+      <div id="yo">
+        <img
+          src="https://media.licdn.com/dms/image/D4E03AQEWxMdwtj22jA/profile-displayphoto-shrink_800_800/0/1693503687958?e=1704326400&v=beta&t=sWCOlqbHwfq0NqAL19ncLV_RiXzqiunFNMirTqtcYw4"
+          class="yo"
+        />
+      </div>
       <h2>Contacts</h2>
       <div>
         <label for="Name">Name:</label>
@@ -44,20 +64,6 @@ export default state => html`
       </div>
       <div id="sub">
         <input type="submit" name="submit" value="Submit" alert="Thank you!" />
-      </div>
-      <div>
-        <h2>
-          About me
-        </h2>
-        <div id="po">
-          <p>a lil about myself</p>
-        </div>
-      </div>
-      <div id="yo">
-        <img
-          src="https://media.licdn.com/dms/image/D4E03AQEWxMdwtj22jA/profile-displayphoto-shrink_800_800/0/1693503687958?e=1704326400&v=beta&t=sWCOlqbHwfq0NqAL19ncLV_RiXzqiunFNMirTqtcYw4"
-          class="yo"
-        />
       </div>
     </form>
   </section>
